69e67c291f0ed6c29781a11e9bd8bfe27c3eeaf0
kukumbala <televna@gmail.com>
2022-10-11 14:52:21 +0300

    Added link on GitHub

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+
+      <footer>
+        This project was coded by Alex and is{" "}
+        <a
+          href="https://github.com/kukumbala/weather_task"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced by GitHub
+        </a>
+      </footer>
     </div>
   );
 }
